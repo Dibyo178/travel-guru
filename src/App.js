8bf447f9b832bc8aa3b1ef0729c95e46ad3958fb
Dibyo178 <Sourov.mu@gmail.com>
2020-09-24 21:12:24 +0600

    finished-work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { createContext, useState } from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './Components/Home/Home';
+import PlaceDetails from './Components/PlaceDetails/PlaceDetails';
+import NavBar from './Components/NavBar/NavBar';
+import Login from './Components/Login/Login';
+import Search from './Components/Search/Search';
+
+import NoMatch from './Components/NoMatch/NoMatch';
+import PrivateRoute from './Components/PrivateRoute/PrivateRoute';
+import './Background.css';
+
+
+
+export const UserContext = createContext()
 
 function App() {
+
+  const [user, setUser] = useState({
+    name: '',
+    first: '',
+    last: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    error: '',
+    success: ''
+  })
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section   className='bgStyle'>
+      <UserContext.Provider value={[user, setUser]}>
+        <Router>
+          <NavBar></NavBar>
+          <Switch>
+            <Route path="/book/:placeId">
+              <PlaceDetails></PlaceDetails>
+            </Route>
+
+            <PrivateRoute path='/search'>
+
+              <Search />
+
+            </PrivateRoute>
+            <Route path='/login'>
+              <Login></Login>
+
+            </Route>
+            <Route path='/home'>
+              <Home></Home>
+
+            </Route>
+            <Route exact path='/'>
+              <Home></Home>
+
+            </Route>
+            <Route path='*/'>
+              <NoMatch></NoMatch>
+
+            </Route>
+          </Switch>
+        </Router>
+
+      </UserContext.Provider>
+    </section>
   );
 }
 
---
+++ src/Components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDeHztSQQQZ6RJynIo0NPDdiltbp7YrGQA",
+  authDomain: "traveler-guru.firebaseapp.com",
+  databaseURL: "https://traveler-guru.firebaseio.com",
+  projectId: "traveler-guru",
+  storageBucket: "traveler-guru.appspot.com",
+  messagingSenderId: "214682968422",
+  appId: "1:214682968422:web:3603cc38df45b594d668a7"
+};
+  export default firebaseConfig;
